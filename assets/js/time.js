795afe887beda0692f7d219bacc62b6f7a92d0e1
Rafael Méndez Pacheco <rafy.pachecomendez@gmail.com>
2022-08-11 16:04:58 -0400

    restaurando dia

--- assets/js/time.js
+++ assets/js/time.js
@@ -6,14 +6,12 @@
 window.onload = displayClock();
 function displayClock() {
 	const monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-	const daysNames = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado'];
 
 	var d = new Date();
 	var mm = monthNames[d.getMonth()];
 	var dd = d.getDate();
 	var min = (mins = ('0' + d.getMinutes()).slice(-2));
 	var hh = d.getHours();
-	var day = daysNames[d.getDay()];
 	var ampm = '';
 
 	if (CONFIG.twelveHourFormat) {
@@ -26,7 +24,6 @@ function displayClock() {
 	document.getElementById('separator').innerHTML = ' : ';
 	document.getElementById('minutes').innerText = min + ampm;
 
-	document.getElementsById('dia').innerText = day;
 	document.getElementById('month').innerText = mm;
 	document.getElementById('day').innerText = dd;
 
